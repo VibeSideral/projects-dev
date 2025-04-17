6b43b384eaca06b182dd814cfe5ab24ffc587abf
VibeSideral <vibesideral@gmail.com>
2025-04-17 18:37:39 -0300

    Estudando com Pseudocodigos

---
+++ Desafio FreeCode/ExerciseJs1/Exercise1.js
@@ -0,0 +1,5 @@
+console.log(`Hello World!
+    This is my first JavaScript program!
+    I am learning JavaScript and I am excited to learn more!
+    My name is Victor and I am 22 years old.`
+);
---
+++ Desafio FreeCode/ExerciseJs2/Exercise2.js
@@ -0,0 +1,13 @@
+const personName = "Victor"
+let age = "23"
+let Occupation = "Student"
+
+console.log(personName)
+console.log(age)
+console.log(Occupation)
+
+//Outra forma de resolver:
+
+//console.log(`${personName} 
+//${age} 
+//${Occupation}`);
